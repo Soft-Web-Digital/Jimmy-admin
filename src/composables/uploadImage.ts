617780a9c327dc100812a5dfc9896b31127d0da4
Sanni David <sannidavidsmart@gmail.com>
2023-11-30 04:44:17 +0100

    implement imagekit

--- src/composables/uploadImage.ts
+++ src/composables/uploadImage.ts
@@ -1,73 +1,47 @@
 export default function uploadFileToImageKit(file: File): Promise<any> {
   return new Promise(function (resolve, reject) {
-    let formData = new FormData();
-    formData.append('file', file);
-    formData.append('fileName', file.name);
 
     let request = new XMLHttpRequest();
-    request.open('POST', 'https://upload.imagekit.io/api/v1/files/upload', true);
-    request.setRequestHeader('Authorization', 'private_ciVlZHgpVTOcXuuX8Qx8KPhjqPo='); // Replace with your ImageKit private API key
+    request.open('POST', import.meta.env.VITE_IMAGEKIT_URL);
+    request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
-    request.onreadystatechange = () => {
-      if (request.readyState === 4) {
+    fetch("https://api.jimmyxchange.com/api/image/signature", {
+      method: "GET"
+    }).then(response => response.json()).then(data => { if (data) callBackFunc(data) }).catch(err => console.log(err))
+
+    function callBackFunc(data: { expire: number, token: string, signature: string }) {
+      let formData = new FormData();
+      formData.append('publicKey', import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY);
+      formData.append('file', file);
+      formData.append('fileName', file.name);
+      formData.append('useUniqueFileName', 'true');
+      formData.append('expire', data.expire.toString());
+      formData.append('token', data.token);
+      formData.append('signature', data.signature);
+
+      request.onreadystatechange = () => {
         // File uploaded successfully
-        if (request.status === 201) {
+        if (request.readyState === 4 && request.status === 200) {
           let response = JSON.parse(request.responseText);
           resolve(response);
-        } else {
-          // Not successful, let's find out what happened
-          let error = JSON.parse(request.responseText);
-          alert('error, status code not 201: ' + error.message);
+        }
+
+        // Not successful, let's find out what happened
+        if (request.status !== 200) {
+          let response = JSON.parse(request.responseText);
+          let error = response.error.message;
+          alert('error, status code not 200 ' + error);
           reject(error);
         }
-      }
-    };
+      };
 
-    request.onerror = (err) => {
-      alert('error: ' + err);
-      reject(err);
-    };
+      request.onerror = (err) => {
+        alert('error: ' + err);
+        reject(err);
+      };
 
-    request.send(formData);
+      request.send(formData);
+    }
+
   });
 }
-
-
-
-
-
-// export default function uploadFileToCloudinary(file: File): Promise<any> {
-//   return new Promise(function (resolve, reject) {
-//     let formData = new FormData();
-//     formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-//     formData.append('folder', import.meta.env.VITE_CLOUDINARY_UPLOAD_FOLDER);
-//     formData.append('file', file);
-
-//     let request = new XMLHttpRequest();
-//     request.open('POST', import.meta.env.VITE_CLOUDINARY_URL, true);
-//     request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-//     request.onreadystatechange = () => {
-//       // File uploaded successfully
-//       if (request.readyState === 4 && request.status === 200) {
-//         let response = JSON.parse(request.responseText);
-//         resolve(response);
-//       }
-
-//       // Not successful, let's find out what happened
-//       if (request.status !== 200) {
-//         let response = JSON.parse(request.responseText);
-//         let error = response.error.message;
-//         alert('error, status code not 200 ' + error);
-//         reject(error);
-//       }
-//     };
-
-//     request.onerror = (err) => {
-//       alert('error: ' + err);
-//       reject(err);
-//     };
-
-//     request.send(formData);
-//   });
-// }
